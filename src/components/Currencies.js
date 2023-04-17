089a6ac9a973ccccca11dfc938e7f351c5b6865d
eric-filishtiner <eric.filishtiner@gmail.com>
2023-04-17 14:11:16 -0400

    Got currency dropdown working pre-style(In the select element display, currencies are surrounded by Currency on the left and parentheses surrounding the currency itself)

--- src/components/Currencies.js
+++ src/components/Currencies.js
@@ -1,57 +1,24 @@
 import React, { useState } from 'react';
 const Currencies = () => {
     const { currency } = useState('');
-    const [current_state, setCurrentState] = useState("Currency $ Dollar");
-    const [current_state2, setCurrentState2] = useState("£ Pound");
-    const [current_state3, setCurrentState3] = useState("€ Euro");
-    const [current_state4, setCurrentState4] = useState("₹ Ruppee");
+    const {chosenCurrency, setChosenCurrency} = useState('Currency($ Dollar)')
 const set_currency = (object) => {
     //console.log(object.target);
     //console.log(object.target.value);
-    setCurrentState("$ Dollar");
-    setCurrentState2("£ Pound");
-    setCurrentState3("€ Euro");
-    setCurrentState4("₹ Ruppee");
-    if(object.target.value === current_state)
-    {
-        console.log("1");
-        setCurrentState('Currency (' + object.target.value + ')');
-    }
-    if(object.target.value === current_state2)
-    {
-        console.log("2");
-        setCurrentState2('Currency (' + object.target.value + ')');
-    }
-    if(object.target.value === current_state3)
-    {
-        console.log("3");
-        setCurrentState3('Currency (' + object.target.value + ')');
-    }
-    if(object.target.value === current_state4)
-    {
-        console.log("4");
-        setCurrentState4('Currency (' + object.target.value + ')');
-    }
-
-    
-
-    console.log(current_state.length);
-}
-const reset_currency = (object) => {
-    setCurrentState("$ Dollar");
-    setCurrentState2("£ Pound");
-    setCurrentState3("€ Euro");
-    setCurrentState4("₹ Ruppee");
-    console.log("all set");
+    object.target.value = "Currency(" + object.target.value + ")";
 }
     return (
         <div className='alert alert-secondary' id = 'outer-div'>
             <span id="currency-span">
-                <select id="currencies" onChange={(event) => set_currency(event)} >
-                    <option value="$ Dollar" label={current_state}></option>
-                    <option value="£ Pound" label={current_state2}></option>
-                    <option value="€ Euro" label={current_state3}></option>
-                    <option value="₹ Ruppee" label={current_state4}></option>
+                <select id="currencies"  onChange={(event) => set_currency(event)} >
+                    <option value="$ Dollar" label="$ Dollar"></option>
+                    <option value="£ Pound" label="£ Pound"></option>
+                    <option value="€ Euro" label="€ Euro"></option>
+                    <option value="₹ Ruppee" label="₹ Ruppee"></option>
+                    <option selected style={{display: 'none'}}value="Currency($ Dollar)" label="Currency($ Dollar)"></option>
+                    <option style={{display: 'none'}} value="Currency(£ Pound)" label="Currency(£ Pound)"></option>
+                    <option style={{display: 'none'}} value="Currency(€ Euro)" label="Currency(€ Euro)"></option>
+                    <option style={{display: 'none'}} value="Currency(₹ Ruppee)" label="Currency(₹ Ruppee)"></option>
                 </select>
             </span>
         </div>
